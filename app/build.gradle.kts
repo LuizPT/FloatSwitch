7b4c88b3a6da7bba8afffe548a66919359d42e0e
LuizPT <95752327+LuizPT@users.noreply.github.com>
2026-09-02 09:34:35 +0100

    chore: preparar versão 0.2 beta 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.diluir.floatswitch"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2-beta1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
